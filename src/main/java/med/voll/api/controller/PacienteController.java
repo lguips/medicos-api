a2eba0f30bcc31500d942a9a185a304def161705
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-10-29 13:01:12 -0300

    response entity return for delete method pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,8 +38,9 @@ public class PacienteController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void inativar(@PathVariable Long id) {
+    public ResponseEntity inativar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.inativar();
+        return ResponseEntity.noContent().build();
     }
 }
