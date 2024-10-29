f5a817eb8961de0a7fd84892c6bf9dd10ccd514e
Luiz Guilherme Pessoa da Silva <lguibtw@gmail.com>
2024-10-29 19:34:31 -0300

    refactor folders and add security class

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,20 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // Indica que é uma classe de configuração para o Spring
+@EnableWebSecurity // Indica que vamos personalizar configurações de segurança.
+public class SecurityConfigurations {
+
+    @Bean // Se não add esse método não expomos o retorno desse método e o Spring não entende/lê, muito importante.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
